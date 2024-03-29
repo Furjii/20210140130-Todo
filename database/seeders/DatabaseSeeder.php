31a0f2621649178d7e3b0b76e2c2baea1a23b40b
Furji <furjimadi@gmail.com>
2024-03-30 01:17:59 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'is_admin' => true,
+            ]
+        );
+        User::factory()->create(
+            [
+                'name' => 'furji',
+                'email' => 'furjimadi@gmail.com',
+            ]
+        );
+        User::factory(100)->create();
+        Todo::factory(500)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
